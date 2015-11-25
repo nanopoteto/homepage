85dd665d7c7cbf2283351c4580a806391220f6d8
nanopoteto <nano.poteto@gmail.com>
2015-11-25 23:47:45 +0900

    initialize repository

---
+++ app/models/task.rb
@@ -0,0 +1,8 @@
+class Task < ActiveRecord::Base
+  belongs_to :project
+
+  validates :title, presence: true
+
+  scope :unfinished, -> { where(done: false)}
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homepage::Application.config.secret_key_base = 'f4daeb4f738b226f0689f1fface64190790ca34bf392ea0def76cff19b0dfa9da3ab00545e06d9ac3663b588a74eb3d7ce2747ea757b149dc9d5bcd9a813cd42'
